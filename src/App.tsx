55615cad25f87e9acd7d0020476fb34b31df71c4
NadyaGolub <nadyagolub@ukr.net>
2024-06-06 12:27:00 +0300

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-
+import { Footer } from "./common/components/footer/footer.component";
+import { Header } from "./common/components/header/header.component";
+import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
+import pizzaMenu from '@app/moсks/plants.json'
 
 
 export const App =() => {
-  return (
-    <h1 className="font-bold text-3xl text-red-500">PizzaStack</h1>
-  );
+  return <div>
+    <Header />
+    <div className="mx-12 mb-24">
+ <MenuList items={pizzaMenu} />
+    </div>
+    <Footer/>
+   
+  </div>
 }
 
 export default App;
--- src/modules/menu/components/menu-list/menu-list.component.tsx
+++ src/modules/menu/components/menu-list/menu-list.component.tsx
@@ -9,7 +9,7 @@ interface MenuListProps {
 
 export const MenuList: FC<MenuListProps> = ({items}) => {
     return (
-        <div className='flex flex-wrap gap-10'>
+        <div className='flex flex-wrap gap-10 justify-center'>
             {items.map(({ image, ...pizza }) => (
                 <MenuItem {...pizza} imagePath={'/assets/pizza/' + image} key={'pizza=${pizza.id}'} />
             ))}
